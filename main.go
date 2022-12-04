c31c07eca7cabb992608ae17ca4dfe00e7d1f716
QUERY-GANI <ahmadxgani@gmail.com>
2022-12-04 21:11:12 +0700

    feat: read json file

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type User struct {
+	Id int
+	Username string
+	Email string
+	Password string
+}
+type Users struct {
+	Users []User
+}
 
 func main() {
-	fmt.Println("Hello Go :v")
+	var users Users
+	file, err := os.ReadFile("./users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(file, &users)
+	fmt.Println(users.Users)
 }
